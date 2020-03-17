afb06f0d7b4ab7ee1af3a248e57a6a3ac6469c6a
Enrique Bermúdez <enbermudez96@gmail.com>
2020-03-17 15:48:05 -0300

    feat: add author username and new badges to template

--- generate.sh
+++ generate.sh
@@ -26,6 +26,9 @@ read -r author_name
 printf "${YELLOW}Author [email]: ${NOCOLOR}"
 read -r author_email
 
+printf "${YELLOW}Author [username]: ${NOCOLOR}"
+read -r author_username
+
 printf "${YELLOW}Codecov [token]: ${NOCOLOR}"
 read -r codecov_token
 
@@ -41,8 +44,8 @@ declare -a license_searchs=("license.year" "license.holder")
 declare -a license_replacements=("$license_year" "$license_holder")
 
 # Replacements on README.md
-declare -a readme_searchs=("project.name" "project.description")
-declare -a readme_replacements=("$project_name" "$project_description")
+declare -a readme_searchs=("project.name" "project.description", "author.username")
+declare -a readme_replacements=("$project_name" "$project_description", "$author_username")
 
 # Replacements on codecov.yml and .circleci/config.yml
 declare -a codecov_searchs=("codecov.token")
--- template/README.md
+++ template/README.md
@@ -1,6 +1,8 @@
 # project.name
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![author.username](https://circleci.com/gh/author.username/project.name.svg?style=shield)](https://circleci.com/gh/author.username/project.name)
+[![codecov](https://codecov.io/gh/author.username/project.name/branch/master/graph/badge.svg)](https://codecov.io/gh/author.username/project.name)
 [![jest](https://jestjs.io/img/jest-badge.svg)](https://github.com/facebook/jest)
 
 project.description
